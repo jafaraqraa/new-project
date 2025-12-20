9220f4641cf63135848e79816d290b4e9b2f441e
jafar <jafarmarwan20@gmail.com>
2025-12-20 20:57:27 +0000

    Auto commit

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+read -p "Are you sure you want to push changes to the repository? (y/n): " choice
+if [[ "$choice" != "y" ]]; then
+    echo "Push operation cancelled."
+    exit 0
+fi
+
 
 
 git add .
